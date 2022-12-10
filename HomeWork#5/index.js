23b7fb4a29dc2dca9e9ce0ce4372db5f065fc059
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2022-12-10 17:39:22 +0100

    Add files via upload

---
+++ HomeWork#5/index.js
@@ -0,0 +1,92 @@
+console.log("-------------------------------------------HOMEWORK#2-----------------------------------------------------------------")
+// THIS IS MY ATTEMPT AT THE HOMEWORK #2
+
+
+// // console.log("CONNECTED");
+
+// // function findNumber (number, array){
+
+// //     let randomNumbers = [3, 4, 456, "John wick", "Spider-Man", 78, 4, "Ant-Man", 4];
+// // }
+
+
+// function tellStory(name, mood, activity){
+
+// console.log(`I am ${name}. Today is my birthday and i am very ${mood}. I will ${activity}.`)
+// }
+
+// tellStory("John", "Happy", "Go to the park");
+
+
+// function arrayOfNumbers (arrayOfFiveNumbers) {
+//     let i = 0;
+//     for ( i = 0; i < arrayOfFiveNumbers.lenght;  i ++ ) {
+//         let finalResult = arrayOfFiveNumbers[i]
+
+//     }
+// }
+// console.log(arrayOfNumbers([1, 2, 3, 4, 5]));
+
+// function arrayOfStrings(){
+//     let i = 0;
+
+//     while( i < arrays)
+//     let arrays = ["My", "Name", "is", "Slave", "and", "I", "like", "to", "play", "Basketball"]
+//     for( i < arrays){
+//         console.log()
+//     }
+    
+
+//     console.log(arrays);
+//     return arrays
+// }
+
+// arrayOfStrings()
+
+function arraysOfNumbers (sum1, sum2, sum3, sum4, sum5) {
+    let calculationSum = [sum1 + sum2 + sum3 + sum4 + sum5];
+    // let randomNumbers = [5, 89, 556, 2, 78];
+    // while(i < sum,  i++){
+    //     console.log(sum)
+    // }
+    // return sum;
+    // return sum;
+}
+// let randomNumbers = [5, 89, 556, 2, 78];
+// let finalResult = arraysOfNumbers()
+// // console.log(finalResult); 
+// let randomNumbers = [5, 89, 556, 2, 78];
+// let finalResult = randomNumbers
+// console.log(arraysOfNumbers(finalResult))
+arraysOfNumbers(485, 888, 121, 88, 6)
+// console.log(arraysOfNumbers)
+
+console.log("---------------------------HOMEWROK#3-------------------------------------------")
+// HAVE NO IDEA
+
+
+
+
+console.log("------------------------------------------------------------------HOMEWORK#4----------------------------------------------------")
+
+// // HOMEWORK #4
+// function loopingNumbers (number){
+//     let i = 0
+//     let orderNumber = 20;
+//     while(i < orderNumber.lenght){
+//         console.log(i)
+
+//      i ++
+//     }
+
+// }
+// loopingNumbers(number)
+
+i = 0
+while(i <= 20){
+    console.log(i)
+    i++
+}
+// I know i didnt do it wright but i lost my mind
+
+//THE HOMEWORKS #5 AND #6 I DIDNT HAVE A CLUE HOW TO DO AND I THINK I HAVE OFFICIALY LOST MY MIND :)
